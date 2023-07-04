a4dc087a2c1dcc728c7bfb8ebddf998ad2746697
Asma-Khanam <128472305+Asma-Khanam@users.noreply.github.com>
2023-07-04 15:14:04 +0530

    Add files via upload

---
+++ Multiplication.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+  using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a;
+  cin >> b;
+  
+  int mult = a * b;
+  
+  cout << mult;
+  
+  return 0;
+}
---
+++ Socks.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    while(T>0){
+      int n;
+      cin>>n;
+      cout<<n+1<<endl;
+      
+      T++;
+    }
+}
